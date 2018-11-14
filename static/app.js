17d514ea476703442bb21fa0bbd46874fe25cbdb
Pavel Djundik <xPaw@users.noreply.github.com>
2018-11-14 23:30:26 +0200

    Set anonymizeIp

--- static/app.js
+++ static/app.js
@@ -505,7 +505,7 @@ catch( e )
 // Only load Google Analytics and Twitter helper widget if DNT is not enabled
 if( !_dntEnabled() )
 {
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);})(win,doc,'script','https://www.google-analytics.com/analytics.js','ga');ga('create','UA-37177069-4','steamstat.us');ga('set','forceSSL',true);ga('send','pageview');
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);})(win,doc,'script','https://www.google-analytics.com/analytics.js','ga');ga('create','UA-37177069-4','auto');ga('set','anonymizeIp',true);ga('send','pageview');
 	(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}})(doc, 'script', 'twitter-wjs');
 }
 }( document, window ) );
